ec879d69e73a671277f0f258d2d977ed47037550
Le Si Son <sison@evol.vn>
2022-12-09 14:54:03 +0700

    fix: lib

---
+++ lib/typescript/PasteInput.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import type { PasteInputProps } from './types';
+declare const _default: React.ForwardRefExoticComponent<PasteInputProps & React.RefAttributes<any>>;
+export default _default;
+//# sourceMappingURL=PasteInput.d.ts.map
